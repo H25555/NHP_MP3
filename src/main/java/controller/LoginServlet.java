e273f4f4d0f9bfc3fce1793860736f3a4cd582e8
H25555 <counterbigdenchet@gmail.com>
2023-06-12 11:28:43 +0700

    add .

---
+++ src/main/java/controller/LoginServlet.java
@@ -0,0 +1,7 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet(name = "loginServlet", urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+}
